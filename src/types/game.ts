bbb23890dc071ffa81c05f9b225b1eebc7519bc3
yishiyang <freeft@gmail.com>
2023-05-14 19:57:17 +0200

    modify interface to include votes field

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,4 +1,41 @@
+import {
+  BggLink,
+  BggName,
+  BggPoll,
+  BggVersion,
+} from "@code-bucket/board-game-geek";
+
 export interface IHotGame {
   id: number;
   name: string;
+}
+
+export interface IBggGame {
+  id: number;
+  type: "boardgame" | string;
+  thumbnail: string;
+  image: string;
+  links: BggLink[];
+  names: BggName[];
+  yearpublished?: number;
+  description: string;
+  minplayers: number;
+  maxplayers: number;
+  polls: BggPoll[];
+  playingtime: number;
+  minplaytime: number;
+  maxplaytime: number;
+  minage: number;
+  versions: BggVersion[];
+  get namesValues(): string[];
+  get primaryName(): string | undefined;
+  get categories(): BggLink[];
+  get mechanics(): BggLink[];
+  get families(): BggLink[];
+  get expansions(): BggLink[];
+  get implementations(): BggLink[];
+  get designers(): BggLink[];
+  get artists(): BggLink[];
+  get publishers(): BggLink[];
+  votes: number;
 }
